27bc87b317b1a5884ab0f17caa8d95233c2c8e61
EpocDotFr <contact@epoc.fr>
2022-09-25 15:58:58 +0200

    Backup Restore solved

--- backup_restore.py
+++ backup_restore.py
@@ -17,45 +17,65 @@ def run_subprocess(arguments):
     subprocess.run(arguments, check=True)
 
 
+def restore_db(dump):
+    with tempfile.NamedTemporaryFile('wb', delete=False, suffix='.sql.gz') as f:
+        f.write(base64.b64decode(dump))
+
+        dump_path = f.name
+
+    run_subprocess((
+        'gunzip',
+        '-d',
+        dump_path
+    ))
+
+    dump_path = dump_path.replace('.gz', '')
+
+    os.putenv('PGPASSWORD', PG_PASSWORD)
+
+    run_subprocess((
+        'psql',
+        PG_DB,
+        '-U',
+        PG_USERNAME,
+        '-c',
+        'DROP TABLE IF EXISTS criminal_records'
+    ))
+
+    run_subprocess((
+        'psql',
+        PG_DB,
+        '-U',
+        PG_USERNAME,
+        '-f',
+        dump_path
+    ))
+
+
+def extract_alive_ssns():
+    conn = psycopg2.connect(dbname=PG_DB, user=PG_USERNAME, password=PG_PASSWORD)
+    cur = conn.cursor()
+
+    cur.execute("SELECT ssn FROM criminal_records WHERE status = 'alive';")
+
+    ret = [
+        row[0] for row in cur.fetchall()
+    ]
+
+    cur.close()
+    conn.close()
+
+    return ret
+
+
 problem = hackattic.Problem('backup_restore')
 
 data = problem.fetch()
 
-with tempfile.NamedTemporaryFile('wb', delete=False, suffix='.sql.gz') as f:
-    f.write(base64.b64decode(data['dump']))
-
-    dump_path = f.name
-
-run_subprocess((
-    'gunzip',
-    '-d',
-    dump_path
-))
-
-dump_path = dump_path.replace('.gz', '')
-
-os.putenv('PGPASSWORD', PG_PASSWORD)
-
-run_subprocess((
-    'psql',
-    PG_DB,
-    '-U',
-    PG_USERNAME,
-    '-c',
-    'DROP TABLE IF EXISTS criminal_records'
-))
-
-run_subprocess((
-    'psql',
-    PG_DB,
-    '-U',
-    PG_USERNAME,
-    '-f',
-    dump_path
-))
+restore_db(data['dump'])
 
 solution = {
-    'alive_ssns': [],
+    'alive_ssns': extract_alive_ssns(),
 }
 
-# print(problem.solve(solution))
+print(problem.solve(solution))
