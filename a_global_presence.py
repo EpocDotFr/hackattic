9e1f27d8c89478675de018da7f78464af03e7967
EpocDotFr <contact@epoc.fr>
2022-10-03 12:32:45 +0200

    Solve A Global Presence

--- a_global_presence.py
+++ a_global_presence.py
@@ -7,22 +7,21 @@ data = problem.fetch()
 
 presence_token = data['presence_token']
 
+# Good luck finding 7 working proxies
 proxies = [
-    ('91.137.33.106', 8080),
-    ('23.82.16.149', 3128),
-    ('64.235.204.107', 3128),
-    ('157.100.12.138', 999),
-    ('173.212.200.30', 3128),
-    ('194.5.193.73', 8080),
-    ('92.43.120.95', 3128),
+    '101.109.176.134:8080',
+    '101.109.176.134:8080',
+    '101.109.176.134:8080',
+    '101.109.176.134:8080',
+    '101.109.176.134:8080',
+    '101.109.176.134:8080',
+    '101.109.176.134:8080',
 ]
 
 for proxy in proxies:
     print(proxy)
 
-    ip, port = proxy
-
-    response = requests.get(f'https://hackattic.com/_/presence/{presence_token}', proxies={'https': f'{ip}:{port}'})
+    response = requests.get(f'https://hackattic.com/_/presence/{presence_token}', proxies={'https': proxy})
     response.raise_for_status()
 
 print(problem.solve({}))
