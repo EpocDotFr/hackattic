6259d56ba48eda8bdc4f2207bde2097c1c24a006
Maxime <EpocDotFr@users.noreply.github.com>
2022-10-05 16:12:41 +0200

    Stuff

--- visual_basic_math.py
+++ visual_basic_math.py
@@ -1,24 +1,10 @@
 # This script requires tesseract to be installed on your system (https://tesseract-ocr.github.io/tessdoc/Installation.html#installation)
 from support import hackattic, download_file
-from PIL import Image
 import pytesseract
 
 
 def read_and_calculate(image_path):
-    image = Image.open(image_path).convert('L')
-
-    width, height = image.size
-
-    for x in range(0, width - 1):
-        for y in range(0, height - 1):
-            current_color = image.getpixel((x, y))
-
-            if current_color != 255:
-                image.putpixel((x, y), 0)
-
-    image.show()
-
-    return pytesseract.image_to_string(image)
+    return pytesseract.image_to_string(image_path)
 
 
 problem = hackattic.Problem('visual_basic_math')
